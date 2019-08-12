c2c269e924bceac305cba22b4a71447e462f520a
JWen00 <Jenniferwen00@gmail.com>
2019-08-12 15:57:40 +1000

    vue is much wow much amaze

--- terminal_typing/typing.js
+++ terminal_typing/typing.js
@@ -2,34 +2,36 @@
 
 var text = "MORE TESTING <br> MORE RESTING MORE TESTING <br> MORE RESTINGMORE TESTING <br> MORE RESTINGMORE TESTING <br> MORE RESTINGMORE TESTING <br> MORE RESTINGMORE TESTING <br> MORE RESTINGMORE TESTING <br> MORE RESTINGMORE TESTING <br> MORE RESTINGMORE TESTING <br> MORE RESTINGMORE TESTING <br> MORE RESTING"; 
 html_element = document.getElementById("text");
-console.log(html_element)
 
-console.log(text.length)
 var i = -1, isTag;
+var showAll = false;
 (function type() { 
     tmp = text.slice(0, ++i);
-    
 
-    
-    // Check if finished typing 
-    console.log(tmp.length) 
-    // ERROR hERE.. It keeps calling type()
     // Does not show the last character
     if (tmp.length === text.length) return; 
-    html_element.innerHTML = tmp + `<span class="blingbling"> &#32;</span>`; 
+
+    // SHows the blinker 
+    html_element.innerHTML = tmp;
+    // `<span class="blingbling"> &#32;</span>`; 
+    
+    if (showAll == true) { 
+        document.getElementById("text").innerHTML = text;
+        return;
+    } 
 
     // Check if we're approaching a 'tag' 
     var last_char = tmp.slice(-1);
     if (last_char === "<") isTag = true; 
     if (last_char === ">") isTag = false; 
-    if (isTag) return type(); 
-    setTimeout(type, 50); 
+    if (isTag) {
+        type();
+    } else setTimeout(type, 50); 
 })(); 
 
-html_element.addEventListener("keypress", showAll())
-
-function showAll() { 
-    console.log("CLickEd BUTTON!")
-    document.getElementById("text").innerHTML = text;
+document.addEventListener("keydown", toggle);
+function toggle() { 
+    showAll = true;
+    console.log("ShowAll is now true");
 };
 
---
+++ terminal_typing/typingREADME.md
@@ -0,0 +1,9 @@
+# Hello! 
+
+This was made to mimic typing on a temminal. Currently, the text is hardcoded in, but you can pass in things dynamically too. 
+
+## Features 
+
+* Each letter is typed out with a 50 millisecond delay 
+* When all the text is showing, it has a consistantly flashing blinker 
+* 

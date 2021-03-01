9c18ddd7bcecd5c69a45f42e9489105f5b377d58
JWen00 <jenniferwen00@gmail.com>
2021-03-01 17:54:54 +1100

    revised terminal typing

--- src/terminal_typing.js
+++ src/terminal_typing.js
@@ -1,22 +1,25 @@
-var sample_text = "This teminal-styled typing effect is really cool!"; 
-html_element = document.getElementById("text");
+var sample_text = "This teminal-styled typing effect is <b>really</b> cool!"; 
+var html_element = document.getElementById("text");
+var blinker = document.getElementById("blinker");
 
 var i = -1, isTag;
 var showAll = false;
-(function type() { 
+var typer = setInterval(function type() { 
     tmp = sample_text.slice(0, ++i);
 
     // Does not show the last character
     if (tmp.length === sample_text.length) return; 
 
     // Shows the blinker 
-    html_element.innerHTML = tmp;
-    // `<span class="blingblin g"> &#32;</span>`; 
-    
-    if (showAll == true) { 
-        document.getElementById("text").innerHTML = sample_text;
-        return;
+    html_element.innerHTML = tmp + "|";
+    if (tmp == sample_text.slice(0, sample_text.length - 1)) { 
+        showAll = true;
     } 
+
+    if (showAll) { 
+        html_element.innerHTML = sample_text;
+    }
+
 
     // Check if we're approaching a 'tag' 
     var last_char = tmp.slice(-1);
@@ -24,10 +27,11 @@ var showAll = false;
     if (last_char === ">") isTag = false; 
     if (isTag) {
         type();
-    } else setTimeout(type, 50); 
-})(); 
+    } else setTimeout(type, 70); 
+}, 1200);
 
-document.addEventListener("keydown", toggle);
+
+document.addEventListener("click", toggle);
 function toggle() { 
     showAll = true;
 };
